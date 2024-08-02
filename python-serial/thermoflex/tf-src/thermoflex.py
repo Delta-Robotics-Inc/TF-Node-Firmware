0d53e427933a4b51b86003446157e054646d253b
mstep44 <marvin.stephens44@yahoo.com>
2024-08-01 19:12:02 -0500

    adding hex skeleton

--- python-serial/thermoflex/tf-src/thermoflex.py
+++ python-serial/thermoflex/tf-src/thermoflex.py
@@ -1,50 +1,36 @@
 import serial.tools.list_ports as stl
 import serial as s
 import time
-# find parsing 
+#TODO: find parsing package
 import pandas as pd
 import matplotlib.pyplot as plt
-import csv
+
 #arduino commands
 
-SE = 'set-enable '
-RESET = 'reset '       #  Make Constants all CAPS
-SM = 'set-mode '
-SS = 'set-setpoint '
-ST = 'status '
-STOP = 'stop '
-LOGMODEM = 'log-mode '
-PRC = 'percent '
-AMP = 'amps '
-VOLT = 'volts '
-DEG =  'degrees '
+SE = "set-enable "
+RESET = "reset "
+SM = "set-mode "
+SS = "set-setpoint "
+ST = "status "
+STOP = "stop "
+LOGMODEM = "log-mode "
+PRC = "percent "
+AMP = "amps "
+VOLT = "volts "
+DEG =  "degrees "
 prt = stl.comports(include_links=False)
 
-hexlist = {"reset":{"all": ' ',"m1": ' ',"m2": ' '},
-           "set-enable":{"all": {True: ' ',False: ' '},
-                 "m1":  {True: ' ',False: ' '},
-                     "m2":  {True: ' ',False: ' '}},
-           "set-mode":{"all": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '},
-               "m1": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '},
-                  "m2": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '}},
-           "set-setpoint":{"all": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '},
-                   "m1": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '},
-                      "m2": {'percent': ' ','amps': ' ', 'volts': ' ','degrees': ' '}},
-           "status":{"all": ' ',"m1": ' ',"m2": ' '}, "stop":0x05,
-           "log-mode":{"all":{0: " ",1: " ",2: " "},
-                   "m1": {0: " ",1: " ",2: " "},
-                       "m2": {0: " ",1: " ",2: " "}}
-                   
-           
-           }
 
+        
+              
+          
 devs = []
 logen = " "
-logout = ''
+logout = ""
 nodel =[]  # Node list 
 prod = [105] # Product id list
-pn = ''  # Port number
-enc = 'utf-8' # Encoding
+pn = ""  # Port number
+enc = "utf-8" # Encoding
 
 def openPort(portnum):
     global arduino
@@ -64,14 +50,87 @@ def closePort(portnum):
     except:
        s.SerialException()
 
-def send_command(x):    
+def send_command_str(x):    
     
-    arduino.write(bytes(x + '\n' , 'utf-8'))
+    arduino.write(bytes(x + "\n" , "utf-8"))
     time.sleep(0.01)
     
-    print('Command sent')
+    print("Command sent")
+
+def send_command(x:int , params:list):
     
+    arduino.write(x, "utf-8")
+    time.sleep(0.01)
+    for p in params:
+        arduino.write(p + "\n", "utf-8")
+        time.sleep(0.01)
+    
+
+class command_t:    
+    global commanddict
+    commanddict = {"reset": 0xFF, "set-enable": 0x01, "set-mode": 0x02, "set-setpoint": 0x03,
+               "status": 0x04, "stop": 0x05, "log-mode":0x06,
+               "all":0x07, "m1":0x08, "m2": 0x09,
+               "percent": 0x10, "amps":0x11, "volts": 0x12,"degree": 0x13,}   
+    
+    
+    def reset(code = commanddict["reset"], device = "all"):
+        send_command(code , [commanddict[device]])
+        
+    
+    def set_enable(state:bool, code = commanddict["set-enable"], device = "all" ):
+        
+        send_command(code , [commanddict[device], state])
+    
+    def set_mode(mode:str, code = commanddict["set-mode"], device = "all" ):
+        
+        sendmode = "" #  make variables more descriptive
+        if mode =="percent":
+            sendmode = commanddict["percent"]
+        elif mode == "amp":
+            sendmode = commanddict["amps"]
+        elif mode == "voltage":
+            sendmode = commanddict["volts"]
+        elif mode == "degree":
+            sendmode = commanddict["degree"]
+        else:
+            print("Error: Incorrect option" )
+            return        
+        
+        send_command(code, [commanddict[device], sendmode])
+    
+    def set_setpoint(mode:str, value:float, code = commanddict["set-setpoint"], device = "all" ):
+        
+        sendmode = "" #  make variables more descriptive
+        if mode =="percent":
+            sendmode = commanddict["percent"]
+        elif mode == "amp":
+            sendmode = commanddict["amps"]
+        elif mode == "voltage":
+            sendmode = commanddict["volts"]
+        elif mode == "degree":
+            sendmode = commanddict["degree"]
+        else:
+            print("Error: Incorrect option" )
+            return
+        
+        send_command(code , [commanddict[device], sendmode, value])
+    
+    def status(code = commanddict["status"], device = "all"):
+       
+        send_command(code , [commanddict[device]])
+        # TODO: recieve data from serial status
+            
+    def stop(code = commanddict["stop"], device = "all"):
+       
+        send_command(code , [commanddict[device]])
    
+    def log_mode(logmode:int, code = commanddict["log-mode"], device = "all"):
+        
+        send_command(code, [commanddict[device], logmode])  
+
+
+
 
 def discover(proid):  # Add to node list here
     global nodel
@@ -105,41 +164,75 @@ class node:
         self.prodid = prodid
         self.port0 = port0
     
+    def testMuscles_str(self):
+        global pn
+        
+        pn = str(self.port0) #find variable to test in the node class
+        openPort(pn)
+        
+        send_command_str(SS + "m1 percent 0.5")
+        send_command_str(SS + "m2 percent 0.5")        
+        #send_command_str("log-mode m1 0")
+        
+      
+        send_command_str("set-enable m1 true")
+        time.sleep(3.0)
+       
+        
+        send_command_str("set-enable m1 false")
+        time.sleep(3.0)
+       
+       
+        send_command_str("set-enable m2 true")
+        time.sleep(3.0)
+      
+        
+        send_command_str("set-enable m2 false")
+        time.sleep(3.0)
+      
+        #send_command_str("log-mode m1 0")
+        
+        print("Test complete")
+        
+        closePort(pn)
+    
     def testMuscles(self):
         global pn
         
         pn = str(self.port0) #find variable to test in the node class
         openPort(pn)
         
-        send_command(SS + 'm1 percent 0.5')
-        send_command(SS + 'm2 percent 0.5')        
-        #send_command("log-mode m1 0")
+        command_t.set_setpoint(mode = "percent", value = 0.5, device = "m1")
+        command_t.set_setpoint(mode = "percent", value = 0.5, device = "m1")        
+        #command_t.log_mode(logmode = 0, device = "m1")
         
       
-        send_command("set-enable m1 true")
+        command_t.set_enable(state = True, device = "m1")
         time.sleep(3.0)
        
         
-        send_command("set-enable m1 false")
+        command_t.set_enable(state = False, device = "m1")
         time.sleep(3.0)
        
        
-        send_command("set-enable m2 true")
+        command_t.set_enable(state = True, device = "m2")
         time.sleep(3.0)
       
         
-        send_command("set-enable m2 false")
+        command_t.set_enable(state = False, device = "m1")
         time.sleep(3.0)
       
-        #send_command("log-mode m1 0")
+        #command_t.log_mode(logmode = 0, device = "m1")
         
-        print('Test complete')
+        print("Test complete")
         
         closePort(pn)
 
         
     def status(self):
-        pass
+        pn = str(self.port0) #find variable to test in the node class
+        openPort(pn)
+        command_t.status()
    
     class muscle:
          def __init__(self, idnum, resistance, diameter, Length):
@@ -150,46 +243,46 @@ class node:
           
          
          def setMode(self, cmode: str):
-             mts = '' #  make variables more descriptive
-             if cmode =='percent':
+             mts = "" #  make variables more descriptive
+             if cmode =="percent":
                  mts = PRC
-             elif cmode == 'amp':
+             elif cmode == "amp":
                  mts = AMP
-             elif cmode == 'voltage':
+             elif cmode == "voltage":
                  mts = VOLT
-             elif cmode == 'degree':
+             elif cmode == "degree":
                  mts = DEG
              else:
-                 print('Error: Incorrect option' )
+                 print("Error: Incorrect option" )
                  return             
              
              
-             send_command(SM + self.idnum +' '+ mts)
+             send_command_str(SM + self.idnum +" "+ mts)
              
          def enable(self):
-             send_command(SE + self.idnum + 'true ')
+             send_command_str(SE + self.idnum + "true ")
              
          def disable(self):
-             send_command(SE + self.idnum + 'false ')
+             send_command_str(SE + self.idnum + "false ")
              
          def setEnable(self, bool):
              if True:
-                 send_command(SE + self.idnum + 'true ')
+                 send_command_str(SE + self.idnum + "true ")
              elif False:
-                 send_command(SE + self.idnum + 'false ')
+                 send_command_str(SE + self.idnum + "false ")
          
          def setSetpoint(self, cmode, spoint):
              
-             send_command(SS + self.idnum + cmode + spoint)      
+             send_command_str(SS + self.idnum + cmode + spoint)      
      
     def getMuscle(ptn): #port number
         pass
     
     def enableAll():
-        send_command(SE + 'all true')
+        send_command_str(SE + "all true")
     
     def disableAll():
-        send_command(SE + 'all false')
+        send_command_str(SE + "all false")
     
     def setMuscle(portnum,musc):
         pass
@@ -203,16 +296,13 @@ class node:
             enc = "binary"
         elif encoding == "utf-8":
             enc = "utf-8"
-        elif encoding == "ASCII":
-            enc = " ascii"
-        elif encoding == "unicode":
-            enc = 'unicode'
+        
         else:
             print("Error: Log encoding")
         
         while True:
             try:
-                buffer = arduino.readline().decode('utf-8').strip()  # Properly decode and strip the data
+                buffer = arduino.readline().decode("utf-8").strip()  # Properly decode and strip the data
                 if not buffer:
                     pass  # Skip if data is empty
                 
