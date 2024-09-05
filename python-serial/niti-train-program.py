0303eb94d64f897399bb815507b408bb179eb3dc
mstep44 <marvin.stephens44@yahoo.com>
2024-09-04 19:08:18 -0500

    working simple commands

--- python-serial/niti-train-program.py
+++ python-serial/niti-train-program.py
@@ -13,21 +13,26 @@ muscle2 = tf.muscle(idnum = 1, resist= 290, diam= 2, length= 145)
 node0.setMuscle(0, muscle1) #takes the mosfet number muscle params to muscle
 node0.setMuscle(1, muscle2)
 
-tf.command_t.log_mode(logmode = 2)  # Set all muscles to fast logmode
+
+node0.status()
+
+node0.logmode = 2
+logcmd = tf.command_t(name = "log-mode", params = [2])  # Set all muscles to fast logmode
 
 
 # Set output path and mode (Like Binary vs UTF-8)
-output_path = "C:/Users/Mark/Documents/TF-Tests/test1.txt"
+output_path = "C:/Users/School/Desktop/obsidian/ThermoFlex/python-serial/test/Log folder/test1.txt"
 
-node0.filepath(output_path)
-node0.logstate["filelog"]=True #sets the logpath and logging to true
+node0.filepath = output_path
+node0.logstate["printlog"]=True #sets the logpath and logging to true
+tf.send_command_str(node0,logcmd)
 
 m_to_train = muscle1  # Just set to the muscle port that should be trained
 
 # Muscle setup
 m_to_train.setMode("percent")  # Train mode does not work yet.  This is the best way for now until we meet and discuss how train mode will work.
 
-m_to_train.setSetpoint("percent", 0.1)  # Dial this value in but start low!  Keep in mind that smoking should occur sometime near the end of the 50 seconds when this value is tuned in.
+m_to_train.setSetpoint(0.1)  # Dial this value in but start low!  Keep in mind that smoking should occur sometime near the end of the 50 seconds when this value is tuned in.
 
 # Specify training program wait values
 wait1 = 50.0
