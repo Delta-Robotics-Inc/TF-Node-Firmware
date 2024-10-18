9016dc61d0702059e01b0fb821df57c615d907ab
MarkDannemiller <privatemtd@gmail.com>
2024-10-18 17:49:46 -0500

    updated state machine

--- src/networking/SerialInterface.cpp
+++ src/networking/SerialInterface.cpp
@@ -1,3 +1,30 @@
+
+
+/**
+ * @file SerialInterface.cpp
+ * @brief Implementation of the SerialInterface class for handling serial communication.
+ *
+ * This file contains the implementation of the SerialInterface class, which provides
+ * methods for sending and receiving packets over a serial interface using the Arduino framework.
+ * The class handles serialization and deserialization of packets, as well as buffering and parsing
+ * incoming data.
+ *
+ * The main functionalities include:
+ * - Sending packets via the `sendPacket` method.
+ * - Receiving and buffering data via the `receiveData` method.
+ * - Parsing buffered data into packets and managing a queue of received packets.
+ * - Checking for available packets and retrieving them using `hasPacket` and `getNextPacket` methods.
+ * - An optional method `receivePacket` that combines receiving data and retrieving the next packet.
+ *
+ * The class also provides a method to get the name of the interface.
+ *
+ * Dependencies:
+ * - Arduino.h: For serial communication functions.
+ * - SerialInterface.h: Header file for the SerialInterface class.
+ *
+ * @note This implementation assumes the existence of a Packet class with methods for serialization
+ *       and deserialization.
+ */
 #include "SerialInterface.h"
 #include "Arduino.h"
 
@@ -12,6 +39,62 @@ void SerialInterface::sendPacket(const Packet& packet) {
     }*/
 }
 
+enum class ReceptionState {
+    WAIT_FOR_START_BYTE,
+    READ_LENGTH,
+    READ_PACKET
+};
+
+ReceptionState state = ReceptionState::WAIT_FOR_START_BYTE;
+std::vector<uint8_t> packetData;
+uint16_t packetLength = 0;
+
+void SerialInterface::receiveData() {
+    while (Serial.available()) {
+        uint8_t byte = Serial.read();
+
+        switch (state) {
+            case ReceptionState::WAIT_FOR_START_BYTE:
+                Serial.println("Waiting for start byte");
+                if (byte == Packet::startByte) {
+                    packetData.clear();
+                    packetData.push_back(byte);
+                    state = ReceptionState::READ_LENGTH;
+                }
+                break;
+
+            case ReceptionState::READ_LENGTH:
+                Serial.println("Reading length");
+                packetData.push_back(byte);
+                if (packetData.size() == 3) { // Start byte + 2 length bytes
+                    packetLength = (packetData[1] << 8) | packetData[2];
+                    state = ReceptionState::READ_PACKET;
+                }
+                break;
+
+            case ReceptionState::READ_PACKET:
+                Serial.print("Reading packet: [");
+                Serial.print(packetData.size() - 3);
+                Serial.print('/');
+                Serial.print(packetLength);
+                Serial.println(']');
+
+                packetData.push_back(byte);
+                if (packetData.size() == 3 + packetLength) { // Start byte + 2 length bytes + packet length
+                    Packet packet;
+                    if (packet.parse(packetData)) {
+                        packetQueue.push(packet);
+                        Serial.println("Packet received");
+                    } else {
+                        Serial.println("Invalid packet");
+                    }
+                    state = ReceptionState::WAIT_FOR_START_BYTE;
+                }
+                break;
+        }
+    }
+}
+/*
 void SerialInterface::receiveData() {
 
     bool sendDebug = Serial.available();  // Debug Received Bytes
@@ -89,7 +172,7 @@ void SerialInterface::receiveData() {
             Serial.println("Invalid packet");
         }
     }
-}
+}*/
 
 bool SerialInterface::hasPacket() {
     return !packetQueue.empty();
