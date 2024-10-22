f59b7b075645f73262eadb9c59fac96f8b8a1e0a
Ayrthon Capili <maxthebooknerd@gmail.com>
2024-10-21 21:37:56 -0500

    Can id change

--- src/networking/CanInterface.cpp
+++ src/networking/CanInterface.cpp
@@ -1,5 +1,4 @@
 #include "CANInterface.h"
-#include "tfnode-messages.h"
 
 CANInterface::CANInterface() {
     if(!CAN.begin(CanBitRate::BR_250k)) {
@@ -8,12 +7,14 @@ CANInterface::CANInterface() {
 }
 
 void CANInterface::sendPacket(const Packet& packet) {
-    tfnode::NodeSettings* nodeSettings;
     std::vector<uint8_t> rawData = packet.serialize();
-    //construct CAN message
-    //uint32_t const can_id = packet.senderId.id[packet.senderId.CANID]; this might be wrong
-    CanMsg msg(CanStandardId(nodeSettings->can_id()), rawData.size(), rawData.data());
-    //Begin sending a CAN packet
+
+    static uint32_t const CAN_ID = 0x20;
+    
+    uint32_t const can_id = packet.senderId.id[packet.senderId.CANID];
+    // Begin sending a CAN packet
+    //TODO ask mark about how to get the ID from the
+    CanMsg msg(CanStandardId(CAN_ID), rawData.size(), rawData.data());
     CAN.write(msg);
 }
 
@@ -22,16 +23,16 @@ void CANInterface::receiveData() {
     // Check if a CAN packet is available and append to rxBuffer
         while (CAN.available()) {
             msg = CAN.read();
-            //the msg is an array so we must process it
             for (int i = 0; i < msg.data_length; i++) {
                 uint8_t byte = msg.data[i];  // Access each byte individually
+                //TODO ask how vectors work
                 rxBuffer.push_back(byte);    // Add each byte to the rxBuffer
             }
         }
 
         // Attempt to parse packets from rxBuffer (same as original)
         while (true) {
-            if (rxBuffer.size() < 4) {
+            if (rxBuffer.size() < 4 ){
                 // Not enough data for even the smallest packet
                 break;
             }
